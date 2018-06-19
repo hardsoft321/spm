e60191519a288a790453862ea926f48e36db24ba
Евгений Первушин <pea@lab321.ru>
2018-06-19 12:17:08 +0600

    Pre_install/post_install scripts added to `pack-create` package template.

--- src/Spm/Cmd/Pack/CreateCmd.php
+++ src/Spm/Cmd/Pack/CreateCmd.php
@@ -48,6 +48,39 @@ class CreateCmd extends Base
         }
         while(!$done);
 
+        $installdefs = array();
+        $INSTALL_QUESTIONS = array(
+            '1' => array('id' => 'pre_install', 'name' => 'pre-install script'),
+            '2' => array('id' => 'post_install', 'name' => 'post-install script'),
+            '3' => array('id' => 'copy', 'name' => 'copy dir'),
+        );
+        do {
+            echo "Installation content (comma-separated) []:\n";
+            foreach($INSTALL_QUESTIONS as $i => $q) {
+                echo " $i) {$q['name']}\n";
+            }
+            $answer = trim(fgets(STDIN));
+            $done = true;
+            if(!empty($answer)) {
+                $items = explode(',', $answer);
+                foreach($items as &$item) {
+                    $item = trim($item);
+                    if(!isset($INSTALL_QUESTIONS[$item])) {
+                        echo "Illegal answer: $item\n";
+                        $done = false;
+                    }
+                }
+                unset($item);
+                if($done) {
+                    foreach($items as $item) {
+                        $q = $INSTALL_QUESTIONS[$item];
+                        $installdefs[$q['id']] = true;
+                    }
+                }
+            }
+        }
+        while(!$done);
+
         $publishedDate = date('Y-m-d');
         $id_name_quoted = var_export($id_name, true);
         $description_quoted = var_export($description, true);
@@ -58,6 +91,16 @@ class CreateCmd extends Base
             echo "Error: manifest.php already exists.\n";
         }
         else {
+            $copy = '';
+            if(!empty($installdefs['copy'])) {
+                $copy = "
+    'copy' => array(
+        array(
+            'from' => '<basepath>/source/copy',
+            'to' => '.'
+        ),
+    ),";
+            }
             file_put_contents('manifest.php', <<<MANIFEST
 <?php
 \$manifest = array(
@@ -72,13 +115,7 @@ class CreateCmd extends Base
     'version' => '1.0.0',
 );
 \$installdefs = array(
-    'id' => '$id_name',
-    'copy' => array(
-        array(
-            'from' => '<basepath>/source/copy',
-            'to' => '.'
-        ),
-    ),
+    'id' => '$id_name',$copy
 );
 
 MANIFEST
@@ -88,13 +125,68 @@ MANIFEST
             }
         }
 
-        echo "Creating source directory ...\n";
-        if(file_exists('source')) {
-            echo "Error: source already exists.\n";
+        if(!empty($installdefs['pre_install']) || !empty($installdefs['post_install'])) {
+            echo "Creating scripts ...\n";
+            if(file_exists('scripts')) {
+                echo "Warning: scripts already exists.\n";
+            }
+            else {
+                mkdir('scripts');
+            }
         }
-        else {
-            mkdir('source');
-            mkdir('source/copy');
+
+        if(!empty($installdefs['pre_install'])) {
+            echo "Creating scripts/pre_install.php ...\n";
+            if(file_exists('scripts/pre_install.php')) {
+                echo "Warning: scripts/pre_install.php already exists.\n";
+            }
+            else {
+                file_put_contents('scripts/pre_install.php', <<<PRE_INSTALL
+<?php
+function pre_install()
+{
+}
+
+PRE_INSTALL
+);
+            }
+        }
+
+        if(!empty($installdefs['post_install'])) {
+            echo "Creating scripts/post_install.php ...\n";
+            if(file_exists('scripts/post_install.php')) {
+                echo "Warning: scripts/post_install.php already exists.\n";
+            }
+            else {
+                file_put_contents('scripts/post_install.php', <<<POST_INSTALL
+<?php
+function post_install()
+{
+}
+
+POST_INSTALL
+);
+            }
+        }
+
+        if(!empty($installdefs['copy'])) {
+            echo "Creating source ...\n";
+            if(file_exists('source')) {
+                echo "Warning: source already exists.\n";
+            }
+            else {
+                mkdir('source');
+            }
+        }
+
+        if(!empty($installdefs['copy'])) {
+            echo "Creating source/copy ...\n";
+            if(file_exists('source/copy')) {
+                echo "Warning: source/copy already exists.\n";
+            }
+            else {
+                mkdir('source/copy');
+            }
         }
 
         echo "Creating README.txt ...\n";
